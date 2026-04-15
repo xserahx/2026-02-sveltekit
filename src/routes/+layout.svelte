874327e74c344146d4ef9ed325155ce82e61e220
Serena Serafini <xxladantessaxx@gmail.com>
2026-04-15 11:26:28 +0200

    all lesson changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import "$lib/styles/reset.css";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<link rel="preconnect" href="https://api.fontshare.com"/>
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,600,700&display=swap" rel="stylesheet"/>	
 </svelte:head>
 
 {@render children()}
