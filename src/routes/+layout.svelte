611a6aeaa484eac42051b5948d5fe6baddf8775a
Serena Serafini <xxladantessaxx@gmail.com>
2026-04-15 12:31:50 +0200

    new changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 
 	<link rel="preconnect" href="https://api.fontshare.com"/>
 	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,600,700&display=swap" rel="stylesheet"/>	
+	<link href="https://api.fontshare.com/v2/css?f[]=inter@400,500,600,700&display=swap" rel="stylesheet"/>
 </svelte:head>
 
 {@render children()}
